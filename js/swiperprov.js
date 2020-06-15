232060480e8b2fc3d21635ad8aea680d16a78a24
Josue <bonillamjosuei@outlook.com>
2020-06-15 11:40:24 -0600

    Creacion Pagina principal y correcciones del menu

---
+++ js/swiperprov.js
@@ -0,0 +1,35 @@
+var swiper1 = new Swiper('.swiper1', {
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination1',
+      clickable: true,
+    },
+  });
+  var swiper2 = new Swiper('.swiper2', {
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination2',
+      clickable: true,
+    },
+  });
+  var swiper3 = new Swiper('.swiper3', {
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination3',
+      clickable: true,
+    },
+  });
+  var swiper4 = new Swiper('.swiper3', {
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination4',
+      clickable: true,
+    },
+  });
+  var swiper5 = new Swiper('.swiper5', {
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination5',
+      clickable: true,
+    },
+  });
